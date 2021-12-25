27abae25d0be9203d5d9996e1ab11ae56f704a90
Dimas Kurniawan <dimaskurniawan40@gmail.com>
2021-12-25 10:42:49 +0700

    Delete all for new Design

--- components/date.js
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns';
-
-export default function Dates({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
-}
--- config/next.config.js
+++
@@ -1,6 +0,0 @@
-const production = process.env.NODE_ENV === 'production';
-
-// export const url = production
-//   ? 'https://kdimas-backend.herokuapp.com/'
-//   : 'http://localhost:1337';
-export const url = 'https://kdimas-backend.herokuapp.com';
